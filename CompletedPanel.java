66e42a0b69028d475d8e3ca1f4392e5208a83486
jerryfang14 <jerrydfang@gmail.com>
2026-01-18 07:38:52 -0500

    Create CompletedPanel.java

---
+++ CompletedPanel.java
@@ -0,0 +1,9 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class CompletedPanel extends JPanel {
+
+    public CompletedPanel() {
+        setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
+    }
+}
